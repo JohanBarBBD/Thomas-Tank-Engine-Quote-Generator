7875636d4294e5728e0591e5c2e182182bf2d46d
Manuel Nunes <manuel.nunes@vcontractor.co.za>
2023-07-18 11:00:40 +0200

    Add Java backend files (Maven);

---
+++ TTTEA/src/main/java/com/tankEngine/TTTEA/TtteaApplication.java
@@ -0,0 +1,13 @@
+package com.tankEngine.TTTEA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtteaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtteaApplication.class, args);
+	}
+
+}
---
+++ TTTEA/src/test/java/com/tankEngine/TTTEA/TtteaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tankEngine.TTTEA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtteaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
