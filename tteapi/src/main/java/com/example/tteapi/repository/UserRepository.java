a2a5eb36c7fcd43888dd8873935b6865a32a02e6
Kyle Pottinger <kylep@bbd.co.za>
2023-07-19 10:18:37 +0200

    feature(API): Added the structure for the API

---
+++ tteapi/src/main/java/com/example/tteapi/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.tteapi.repository;
+import com.example.tteapi.model.User;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+}
