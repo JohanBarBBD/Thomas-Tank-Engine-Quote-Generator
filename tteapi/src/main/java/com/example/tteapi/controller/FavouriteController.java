5d0069771964de9e9f02196fd1d0597333fe1001
Matthew Dacre <matthewd@bbd.co.za>
2023-07-27 11:50:21 +0200

    Extracting utils to singleton class

--- tteapi/src/main/java/com/example/tteapi/controller/FavouriteController.java
+++ tteapi/src/main/java/com/example/tteapi/controller/FavouriteController.java
@@ -3,16 +3,12 @@ package com.example.tteapi.controller;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.example.tteapi.model.Favourite;
-import com.example.tteapi.model.Quote;
-import com.example.tteapi.model.User;
 import com.example.tteapi.repository.FavouriteRepository;
 import com.example.tteapi.repository.QuoteRepository;
 import com.example.tteapi.repository.UserRepository;
