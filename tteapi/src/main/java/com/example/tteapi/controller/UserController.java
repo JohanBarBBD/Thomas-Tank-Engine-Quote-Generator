ff67d8cd724bec60587e28388f1ed8a3cafc434a
Kyle Pottinger <kylep@bbd.co.za>
2023-07-27 11:49:26 +0200

    feature(oauth-endpoint): Created the endpoint to handle the validation of the google idToken

--- tteapi/src/main/java/com/example/tteapi/controller/UserController.java
+++ tteapi/src/main/java/com/example/tteapi/controller/UserController.java
@@ -1,6 +1,10 @@
 package com.example.tteapi.controller;
 
+import java.security.Key;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +15,15 @@ import org.springframework.web.bind.annotation.*;
 
 import com.example.tteapi.model.User;
 import com.example.tteapi.repository.UserRepository;
+import com.google.api.client.auth.openidconnect.IdToken.Payload;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 
 @CrossOrigin(origins = "http://localhost:8081")
 @RestController
@@ -19,6 +32,33 @@ public class UserController {
 
 	@Autowired
 	UserRepository userRepository;
+
+	@PostMapping("/users/login")
+	public ResponseEntity<String> handleGoogleAuth(@RequestBody String idToken) {
+        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), JacksonFactory.getDefaultInstance())
+		.setAudience(Collections.singletonList(System.getenv("CLIENT_ID")))
+		.build();
+
+        GoogleIdToken idTokenObject = null;
+        try {
+            idTokenObject = verifier.verify(idToken);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid token");
+        }
+
+        if (idTokenObject != null) {
+			Payload payload = idTokenObject.getPayload();
+
+			String userId = payload.getSubject();
+			String email = (String) payload.get("email");
+			String name = (String) payload.get("name");
+
+            String jwtToken = generateJwtToken(userId, email, name);
+            return ResponseEntity.ok(jwtToken);
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid token");
+        }
+    }
 
 	@GetMapping("/users")
 	public ResponseEntity<List<User>> getAllUsers() {
@@ -75,4 +115,21 @@ public class UserController {
 			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
+
+	public static String generateJwtToken(String userId, String userEmail, String userName) {
+		
+		Key key = Keys.hmacShaKeyFor(System.getenv("JWT_SECRET").getBytes());
+		
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + 3600000);
+
+        return Jwts.builder()
+                .setSubject(userId)
+                .claim("email", userEmail)
+                .claim("name", userName)
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
 }
