40e6a2105f81acf3cb63dcd7aa013584ed05ffef
JohanBarBBD <122454216+JohanBarBBD@users.noreply.github.com>
2023-07-27 21:04:18 +0200

    changes

--- public/scripts/landing.js
+++ public/scripts/landing.js
@@ -1,23 +1,17 @@
 function extractTokenFromURL() {
-    const urlParams = new URLSearchParams(location.search);
-    const token = urlParams.get("Token");
+  const urlParams = new URLSearchParams(location.search);
+  const token = urlParams.get("Token");
 
-    if (token) {
-        localStorage.setItem("token", token);
-        let decoded = parseJwt(token)
-        localStorage.setItem("user",decoded.email)
-    } else {
-        console.log("Token not found in URL.");
-    }
+  if (token) {
+    localStorage.setItem('token', token);
+    let decoded = parseJwt(token);
+    localStorage.setItem('user', decoded.email);
+  } else {
+    console.log('Token not found in URL.');
+  }
 }
-extractTokenFromURL()
 
-window.addEventListener('load', async() =>{
-    const token = localStorage.getItem("token");
-    if (!token) {
-        window.location.href = '../index.html';
-    }
-});
+extractTokenFromURL();
 
 function parseJwt (token) {
     let base64Url = token.split('.')[1];
@@ -30,24 +24,29 @@ function parseJwt (token) {
     
 }
 
-const apiUrl = 'http://tteapi-4-env.eba-7w3sxei8.af-south-1.elasticbeanstalk.com'
+const apiUrl ='https://tteapi-4-env.eba-7w3sxei8.af-south-1.elasticbeanstalk.com';
+const userQueryParam = new URLSearchParams({ email: localStorage.getItem('user')
+})
+const tokenQueryParam = new URLSearchParams({ token: localStorage.getItem('token')})
 
 async function getFavorites() {
-    console.log(`${apiUrl}/api/favourites`);
-    try{
-      const response = await fetch(`${apiUrl}/api/favourites`)
-        const myJson = await response.json();
-        let list = document.getElementById('Favorites');
-        for(let i =0; i< myJson.length; i++){
-          let listItem = document.createElement('li');
-          listItem.innerText=`${myJson[i]}`;
-          list.appendChild(listItem);
-        }
-  
-    }catch(err){  
-      alert("Technical error try again later.")
+  const userIdResponse = await fetch(
+    `${apiUrl}/api/users/by-email?${userQueryParam} & ${tokenQueryParam}`
+  );
+  const userIdJson = await userIdResponse.json();
+  const userId = userIdJson.id;
+  const favoritesResponse = await fetch(
+      `${apiUrl}/api/favourites/${userId}?${tokenQueryParam}`
+  );
+  const myJson = await favoritesResponse.json();
+  let list = document.getElementById('Favorites');
+  for (let i = 0; i < myJson.length; i++) {
+    let listItem = document.createElement('li');
+    listItem.innerText = `${myJson[i]}`;
+    list.appendChild(listItem);
+
     }
-  
   }
 
-getFavorites();
+getFavorites()
+
